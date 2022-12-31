ed2da09dd906389dc2e2c34102fe98587d1c27ed
LassTwoKey <dimas.super29@gmail.com>
2022-12-31 13:54:38 +0300

    add typescript

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import "normalize.css";
+import "./styles/index.scss";
+
+const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
+root.render(<App />);
