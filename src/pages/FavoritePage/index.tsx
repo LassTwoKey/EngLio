5b172645665b729113102a773ac2679cfa8e8e6b
LassTwoKey <dimas.super29@gmail.com>
2023-01-06 15:55:44 +0300

    change FavoritePage

--- src/pages/FavoritePage/index.tsx
+++ src/pages/FavoritePage/index.tsx
@@ -1,10 +1,64 @@
 import { FC } from "react";
 import { useParams } from "react-router-dom";
+import cn from "classnames";
 import PageWrapper from "../../ui/PageWrapper";
+import Typography from "../../ui/Typography";
+import Button from "../../ui/Button";
+import { data } from "../../mock/favorites";
+
+import styles from "./index.module.scss";
 
 const FavoritePage: FC = () => {
   const { id } = useParams();
-  return <PageWrapper title={id} goBack></PageWrapper>;
+  if (!id) return null;
+
+  const favoriteName = data[id].name;
+  const favoriteItems = data[id].list;
+
+  let content;
+
+  content = (
+    <ul>
+      {favoriteItems.map(favoriteItem => (
+        <div
+          key={favoriteItem.id}
+          className={cn("d-flex jc-between py-4", styles.item)}
+        >
+          <div className="">
+            <div className="d-flex ai-center">
+              <Typography tag="h3" className="mr-2">
+                {favoriteItem.word}
+              </Typography>
+              {favoriteItem.transcription && (
+                <Typography tag="p">{favoriteItem.transcription}</Typography>
+              )}
+            </div>
+            <Typography tag="p">{favoriteItem.translate}</Typography>
+          </div>
+          <div className="d-flex ai-center">
+            <Button type="outlined" className="mr-2">
+              Сложное
+            </Button>
+            <Button type="primary">Убрать</Button>
+          </div>
+        </div>
+      ))}
+    </ul>
+  );
+
+  if (favoriteItems.length === 0) {
+    content = (
+      <Typography tag="h3" isCenter>
+        Список пуст
+      </Typography>
+    );
+  }
+
+  return (
+    <PageWrapper title={favoriteName} goBack>
+      <div className="container">{content}</div>
+    </PageWrapper>
+  );
 };
 
 export default FavoritePage;
